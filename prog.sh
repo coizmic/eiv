630883bdd9ee5dc9bc79d00549ecbc4c65c1e634
coizmic <28333510+coizmic@users.noreply.github.com>
2022-07-09 16:19:20 +0900

    HoT: initial revision

---
+++ prog.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+echo "Tossing a coin..."
+
+CNT_TAIL=0
+CNT_HEAD=0
+
+for i in {1..3}; do
+  if [ "$(shuf -i 0-1 -n 1)" == 0 ]; then
+    COIN="Tails"
+    CNT_TAIL=$((CNT_TAIL + 1))
+  else 
+    COIN="Heads"
+    CNT_HEAD=$((CNT_HEAD + 1))
+  fi
+  echo "Round $i: $COIN"
+done
+
+echo "Heads: $CNT_HEAD, Tails: $CNT_TAIL"
