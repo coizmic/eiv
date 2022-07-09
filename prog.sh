cb011bae3a95ddcdab08f50a7e89872be848fb94
coizmic <28333510+coizmic@users.noreply.github.com>
2022-07-09 16:29:41 +0900

    User name added

--- prog.sh
+++ prog.sh
@@ -5,6 +5,10 @@ echo "Tossing a coin..."
 CNT_TAIL=0
 CNT_HEAD=0
 
+echo "Who are you?"
+read -p "> " NAME
+echo "Hello, $NAME!"
+
 for i in {1..3}; do
   if [ "$(shuf -i 0-1 -n 1)" == 0 ]; then
     COIN="Tails"
